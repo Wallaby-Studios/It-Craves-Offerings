ee534ffcca9142325383aef672f17c9d4895a44f
Rhys Stever <rhys@stever.com>
2025-02-22 00:47:26 -0500

    Fix doors

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -16,7 +16,7 @@ public class Door : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        unlocked = false;
+        
     }
 
     // Update is called once per frame
